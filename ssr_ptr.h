879acd76077e8d3fa128f01daf37da24c69bde6f
young_tea <3h.young.tea@gmail.com>
2026-01-02 01:01:26 +0800

    Rename ssr_ptr class to tea and update methods

--- ssr_ptr.h
+++ ssr_ptr.h
@@ -1,19 +1,19 @@
-#ifndef	ssr_ptr_h
-#define	ssr_ptr_h
+#ifndef	tea_h
+#define	tea_h
 #include<bits/stdc++.h>
 namespace yt{
 template<class ty>
-class ssr_ptr{
+class tea{
 	ty *x;
 public:
-	ssr_ptr(void);
-	ssr_ptr(ssr_ptr&)=delete;
-	ssr_ptr(const ssr_ptr&)=delete;
-	ssr_ptr(ssr_ptr&&);
-	~ssr_ptr(void);
-	auto operator=(ssr_ptr&)=delete;
-	auto operator=(const ssr_ptr&)=delete;
-	auto operator=(ssr_ptr&&)->ssr_ptr&;
+	tea(void);
+	tea(tea&)=delete;
+	tea(const tea&)=delete;
+	tea(tea&&);
+	~tea(void);
+	auto operator=(tea&)=delete;
+	auto operator=(const tea&)=delete;
+	auto operator=(tea&&)->tea&;
 	template<class...args>
 	void make(args&&...);
 	void remove(void);
@@ -23,20 +23,20 @@ public:
 	auto operator->(void)const->const ty*;
 };
 template<class ty>
-ssr_ptr<ty>::ssr_ptr(void){
+tea<ty>::tea(void){
 	x=nullptr;
 }
 template<class ty>
-ssr_ptr<ty>::ssr_ptr(ssr_ptr &&y){
+tea<ty>::tea(tea &&y){
 	x=y.x;
 	y.x=nullptr;
 }
 template<class ty>
-ssr_ptr<ty>::~ssr_ptr(void){
+tea<ty>::~tea(void){
 	remove();
 }
 template<class ty>
-auto	ssr_ptr<ty>::operator=(ssr_ptr &&y)->ssr_ptr&{
+auto	tea<ty>::operator=(tea &&y)->tea&{
 	if(this==&y)
 		return *this;
 	remove();
@@ -46,31 +46,31 @@ auto	ssr_ptr<ty>::operator=(ssr_ptr &&y)->ssr_ptr&{
 }
 template<class ty>
 template<class...args>
-void	ssr_ptr<ty>::make(args&&...arg){
+void	tea<ty>::make(args&&...arg){
 	remove();
 	x=new ty(std::forward<args>(arg)...);
 }
 template<class ty>
-void	ssr_ptr<ty>::remove(void){
+void	tea<ty>::remove(void){
 	if(x==nullptr)
 		return;
 	delete x;
 	x=nullptr;
 }
 template<class ty>
-auto	ssr_ptr<ty>::empty(void)->bool{
+auto	tea<ty>::empty(void)->bool{
 	return x==nullptr;
 }
 template<class ty>
-auto	ssr_ptr<ty>::is_valid(void)->bool{
+auto	tea<ty>::is_valid(void)->bool{
 	return x!=nullptr;
 }
 template<class ty>
-auto	ssr_ptr<ty>::operator->(void)->ty*{
+auto	tea<ty>::operator->(void)->ty*{
 	return x;
 }
 template<class ty>
-auto	ssr_ptr<ty>::operator->(void)const->const ty*{
+auto	tea<ty>::operator->(void)const->const ty*{
 	return x;
 }
 }
